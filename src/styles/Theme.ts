a7379b6235ce8ec1f1a4b82fa93c527b6634689e
yoon-junseo <ysj6844@naver.com>
2022-03-20 16:40:40 +0900

    feat: 기본 스타일 설정
    1. Theme
    2. GlobalStyle
    3. font
    4. App에 반영
    #2

---
+++ src/styles/Theme.ts
@@ -0,0 +1,20 @@
+export const Theme = {
+  // main
+  M_1: '#D9F2FC',
+  M_2: '#3FC06E',
+  M_3: '#F95E64',
+  M_4: '#FFAD72',
+  M_5: '#FFD66A',
+
+  // background
+  B_1: '#FFFFFF',
+  B_2: '#F0F0F0',
+  B_3: '#E0E0E0',
+  B_4: '#EEEEEE',
+  B_5: '#CCCCCC',
+
+  // text
+  T_1: '#000000',
+  T_2: '#8E8E93',
+  T_3: '#A8A8A8',
+};
