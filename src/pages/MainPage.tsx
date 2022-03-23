541e5bb8ae0bace46cfa664d12a6ba46cbe1c7e1
yoon-junseo <ysj6844@naver.com>
2022-03-23 20:07:04 +0900

    feat: MainPage에 컴포넌트 사용 방법 추가
    #6

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,37 @@
 import React, { useState } from 'react';
 
-import { questionList } from '@/lib/constants';
+import { questionList, delayedTimeList } from '@/lib/constants';
 
 import QuestionItem from '@/components/Survey/QuestionItem';
 import Title from '@/components/Survey/Title';
 
+import DelayedTime from '@/components/Exam/DelayedTime';
+
+import Input from '@/components/Comment/Input';
+import CharacterSelection from '@/components/Comment/CharacterSelection';
+import CommentInput from '@/components/Comment/CommentInput';
+
+import Icon from '@/components/Icon';
+
 const MainPage = () => {
   const [clickedQuestionNumber, setClickedQuestionNumber] = useState<number>(0);
+  const [clickedDelayedTimeNumber, setClickedDelayedTimeNumber] = useState<number>(0);
+  const [comment, setComment] = useState<string>('');
+  const init = clickedDelayedTimeNumber === 0;
 
   const onClickQuestion = (number: number) => {
     setClickedQuestionNumber(number);
   };
+
+  const onClickDelayedTime = (number: number) => {
+    setClickedDelayedTimeNumber(number);
+  };
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  };
+
+  const onClickButton = () => {};
 
   return (
     <div>
@@ -28,6 +49,21 @@ const MainPage = () => {
           ))}
         </>
       ))}
+      <CharacterSelection />
+
+      {delayedTimeList.map((time, index) => (
+        <DelayedTime
+          init={init}
+          number={index + 1}
+          clickedDelayedTimeNumber={clickedDelayedTimeNumber}
+          onClickItem={() => onClickDelayedTime(index + 1)}
+        >
+          {time}
+        </DelayedTime>
+      ))}
+      <Icon icon="Arrow" color="#8E8E93" rotate={270} width={12} height={7} />
+      <Input value={comment} onChangeInput={onChangeInput} onClickButton={onClickButton} />
+      <CommentInput value={comment} onChangeInput={onChangeInput} onClickButton={onClickButton} />
     </div>
   );
 };
