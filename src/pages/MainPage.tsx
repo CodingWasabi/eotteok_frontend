9a4ff9779d1e72b56e09f4195a47b579d5f746f0
yoon-junseo <ysj6844@naver.com>
2022-03-23 09:15:07 +0900

    feat: MainPage에 질문 항목 작성
    #6

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { questionList } from '@/lib/constants';
+
+import QuestionItem from '@/components/Survey/QuestionItem';
+import Title from '@/components/Survey/Title';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const [clickedQuestionNumber, setClickedQuestionNumber] = useState<number>(0);
+
+  const onClickQuestion = (number: number) => {
+    setClickedQuestionNumber(number);
+  };
+
+  return (
+    <div>
+      {questionList.map(({ title, question }) => (
+        <>
+          <Title>{title}</Title>
+          {question.map((item, index) => (
+            <QuestionItem
+              number={index + 1}
+              clickedQuestionNumber={clickedQuestionNumber}
+              onClickItem={() => onClickQuestion(index + 1)}
+            >
+              {item}
+            </QuestionItem>
+          ))}
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default MainPage;
