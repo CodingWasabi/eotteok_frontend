2d81b3138019ac4cd2cdd39f342d0eb5b4d2cfff
yoon-junseo <ysj6844@naver.com>
2022-03-24 08:01:37 +0900

    feat: 아이콘 추가
    1. 로고
    2. 연필 로고
    #8

--- src/components/Icon/icons/index.tsx
+++ src/components/Icon/icons/index.tsx
@@ -3,3 +3,5 @@ export { default as Share } from './Share';
 export { default as Arrow } from './Arrow';
 export { default as Send } from './Send';
 export { default as QuestionMark } from './QuestionMark';
+export { default as Logo } from './Logo';
+export { default as LogoPencil } from './LogoPencil';
