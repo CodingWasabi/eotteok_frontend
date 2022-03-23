cfda4b1428c46e8a66555c138ec3f40b4eafb813
yoon-junseo <ysj6844@naver.com>
2022-03-24 08:06:53 +0900

    fix: MainPage 구성
    1. 위치 옮김
    2. 페이지 구성
    #8

---
+++ src/pages/MainPage/style.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Body = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+
+  & > :first-child {
+    margin-bottom: 37px;
+  }
+
+  & > :nth-child(2) {
+    margin-bottom: 66px;
+  }
+
+  & > :nth-child(3) {
+    margin-bottom: 66px;
+  }
+`;
+
+export const LogoWrapper = styled.div`
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 170px;
+`;
+
+export const LogoPencilWrapper = styled.div`
+  position: absolute;
+  right: 25px;
+  top: -8px;
+`;
