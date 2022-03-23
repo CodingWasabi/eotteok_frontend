9b3df7d5260e7c025002962c641e504be9214d95
yoon-junseo <ysj6844@naver.com>
2022-03-23 19:28:44 +0900

    fix: constants
    1. 코드 제거
    #6

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,6 +1,3 @@
 export { default as colorList } from './colorList';
 export { default as questionList } from './questionList';
-<<<<<<< Updated upstream
-=======
 export { default as delayedTimeList } from './delayedTimeList';
->>>>>>> Stashed changes
