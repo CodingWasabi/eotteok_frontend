b2e09ddd5cce0c0e71de3a0dce72d4200b6a95c2
yoon-junseo <ysj6844@naver.com>
2022-03-23 15:18:29 +0900

    feat: constant 추가
    1. delayedTimeList -> 시험 미룬 시간 배열
    #6

---
+++ src/lib/constants/delayedTimeList.ts
@@ -0,0 +1,3 @@
+const delayedTimeList = ['10시간 이내', '20시간', '30시간', '40시간', '50시간 이상'];
+
+export default delayedTimeList;
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,6 @@
 export { default as colorList } from './colorList';
 export { default as questionList } from './questionList';
+<<<<<<< Updated upstream
+=======
+export { default as delayedTimeList } from './delayedTimeList';
+>>>>>>> Stashed changes
