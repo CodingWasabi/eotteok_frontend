4f1c0858440e71866d1905f8bd355449d109e1f2
yoon-junseo <ysj6844@naver.com>
2022-03-24 08:06:27 +0900

    fix Nickname/Input
    1. props 추가
    2. 폰트 크기 및 간격 조정
    #8

--- src/components/Nickname/Input/index.stories.tsx
+++ src/components/Nickname/Input/index.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 
-import Input from '.';
+import Input, { IInputProps } from '.';
 
 export default {
   component: Input,
   title: 'Nickname/Input',
 };
 
-const Template: Story = () => <Input />;
+const Template: Story<IInputProps> = (args: IInputProps) => <Input {...args} />;
 
 export const Default = Template.bind({});
