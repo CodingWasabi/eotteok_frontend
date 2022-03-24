85dad2b599aa6845b7eee7772e5f4ed63031f6eb
yoon-junseo <ysj6844@naver.com>
2022-03-24 16:55:33 +0900

    feat: 루트 페이지에 redux 연결
    #10

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-import App from './components/App';
+import rootReducer from '@/modules';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from '@/components/App';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root'),
+);
