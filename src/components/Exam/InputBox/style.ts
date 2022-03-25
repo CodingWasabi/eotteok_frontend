13ba44b49cc1a74208e790608a24e8d4bf4ead8a
yoon-junseo <ysj6844@naver.com>
2022-03-26 08:59:49 +0900

    fix: InputBox
    1. isFilled 추가 -> border 색상 변경을 위함
    2. onClick 추가
    #12

--- src/components/Exam/InputBox/style.ts
+++ src/components/Exam/InputBox/style.ts
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
+import { IInputBoxProps } from '.';
+
 import { Theme } from '@/styles/Theme';
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.div<IInputBoxProps>`
+  cursor: pointer;
+
   display: flex;
   align-items: center;
   justify-content: center;
@@ -11,6 +15,6 @@ export const Wrapper = styled.div`
   height: 45px;
 
   background-color: ${Theme.B_1};
-  border: 1px solid ${Theme.T_2};
+  border: ${({ isFilled }) => (isFilled ? `1px solid ${Theme.M_2}` : `1px solid ${Theme.T_2}`)};
   border-radius: 12px;
 `;
