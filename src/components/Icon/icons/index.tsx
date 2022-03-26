ff460da76d67c2f52a7e9bed9ac99dc5630c832a
yoon-junseo <ysj6844@naver.com>
2022-03-26 09:00:16 +0900

    feat: icon 추가
    1. FilledArrow -> 날짜 선택, 시간 선택 컴포넌트에서 사용
    #12

--- src/components/Icon/icons/index.tsx
+++ src/components/Icon/icons/index.tsx
@@ -5,3 +5,4 @@ export { default as Send } from './Send';
 export { default as QuestionMark } from './QuestionMark';
 export { default as Logo } from './Logo';
 export { default as LogoPencil } from './LogoPencil';
+export { default as FilledArrow } from './FilledArrow';
