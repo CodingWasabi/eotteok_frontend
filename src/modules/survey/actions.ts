eccfa83317ff8dbd66db8751cf54acc32ee060dd
yoon-junseo <ysj6844@naver.com>
2022-03-29 11:48:44 +0900

    feat: survey store에 내용 추가
    1. initialState의 날짜와 시간을 현재 시간으로 설정
    2. action들 추가
    3. type들 추가
    #12

--- src/modules/survey/actions.ts
+++ src/modules/survey/actions.ts
@@ -1,11 +1,25 @@
 import { createAction } from 'typesafe-actions';
 
+import { IsFilledType, ExamScheduleType, ExamInfoType, ExamInfoIdType } from '@/modules/survey';
+
 /**
  * 액션 타입
  */
 const HANDLE_ANSWER_LIST = 'survey/HANDLE_ANSWER_LIST';
+const UPDATE_IS_FILLED = 'survey/UPDATE_IS_FILLED';
+const UPDATE_EXAM_SCHEDULE = 'survey/UPDATE_EXAM_SCHEDULE';
+const REGISTER_EXAM_INFO = 'survey/REGISTER_EXAM_INFO';
+const INITIALIZE_EXAM_INFO = 'survey/INITIALIZE_EXAM_INFO';
+const UPDATE_EXAM_INFO_ID = 'survey/UPDATE_EXAM_INFO_ID';
+const EDIT_EXAM_INFO = 'survey/EDIT_EXAM_INFO';
 
 /**
  * 액션 생성 함수
  */
 export const handleAnswerList = createAction(HANDLE_ANSWER_LIST)<Array<number>>();
+export const updateIsFilled = createAction(UPDATE_IS_FILLED)<IsFilledType>();
+export const updateExamSchedule = createAction(UPDATE_EXAM_SCHEDULE)<ExamScheduleType>();
+export const registerExamInfo = createAction(REGISTER_EXAM_INFO)<ExamInfoType>();
+export const initializeExamInfo = createAction(INITIALIZE_EXAM_INFO)();
+export const updateExamInfoId = createAction(UPDATE_EXAM_INFO_ID)<number>();
+export const editExamInfo = createAction(EDIT_EXAM_INFO)<ExamInfoType & ExamInfoIdType>();
--- src/modules/survey/reducer.ts
+++ src/modules/survey/reducer.ts
@@ -1,19 +1,86 @@
 import { createReducer } from 'typesafe-actions';
 
-import { handleAnswerList } from '@/modules/survey/actions';
+import {
+  handleAnswerList,
+  updateIsFilled,
+  updateExamSchedule,
+  registerExamInfo,
+  initializeExamInfo,
+  updateExamInfoId,
+  editExamInfo,
+} from '@/modules/survey/actions';
 import { SurveyType, SurveyAction } from '@/modules/survey/types';
+
+const date = new Date();
 
 const initialState: SurveyType = {
   answerList: [],
-  subjectInfoList: [],
+
+  exam: '',
+  examInfoId: -1,
+  examInfoList: [],
+
+  year: date.getFullYear(),
+  month: date.getMonth() + 1,
+  date: date.getDate(),
+
+  hour: date.getHours(),
+  minute: date.getMinutes(),
+
+  prepareTime: 0,
+
+  isEdit: false,
+
+  isFilledDate: false,
+  isFilledTime: false,
+  isFilledPrepareTime: false,
 };
 
-const survey = createReducer<SurveyType, SurveyAction>(initialState).handleAction(
-  handleAnswerList,
-  (state, action) => ({
+const survey = createReducer<SurveyType, SurveyAction>(initialState)
+  .handleAction(handleAnswerList, (state, { payload: answerList }) => ({
     ...state,
-    answerList: action.payload,
-  }),
-);
+    answerList,
+  }))
+  .handleAction(updateIsFilled, (state, { payload: { target, isFilled } }) => ({
+    ...state,
+    [target]: isFilled,
+  }))
+  .handleAction(updateExamSchedule, (state, { payload: { target, value } }) => ({
+    ...state,
+    [target]: value,
+  }))
+  .handleAction(registerExamInfo, (state, { payload: { exam, year, month, date, hour, minute, prepareTime } }) => ({
+    ...state,
+    examInfoList: state.examInfoList.concat({
+      exam,
+      year,
+      month,
+      date,
+      hour,
+      minute,
+      prepareTime,
+    }),
+  }))
+  .handleAction(initializeExamInfo, (state) => ({
+    ...state,
+    exam: '',
+    year: new Date().getFullYear(),
+    month: new Date().getMonth() + 1,
+    date: new Date().getDate(),
+    hour: new Date().getHours(),
+    minute: new Date().getMinutes(),
+    prepareTime: 0,
+    isFilledDate: false,
+    isFilledTime: false,
+    isFilledPrepareTime: false,
+  }))
+  .handleAction(updateExamInfoId, (state, { payload: examInfoId }) => ({
+    ...state,
+    examInfoId,
+  }))
+  .handleAction(editExamInfo, (state, { payload: { examInfoId, ...examInfo } }) => ({
+    ...state,
+    examInfoList: state.examInfoList.map((info, index) => (index === examInfoId ? examInfo : info)),
+  }));
 
 export default survey;
