75e96ab34b745659defdd422665e19dad798acdb
yoon-junseo <ysj6844@naver.com>
2022-03-29 15:26:33 +0900

    feat: Calendar
    1. 달력 컴포넌트
    2. 스타일
    3. 스토리북
    + 시험 준비 기간에 border 추가하기
    + 댓글이 있는 경우 댓글 캐릭터 표시하기
    #1

---
+++ src/components/Calendar/style.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+import { Theme } from '@/styles/Theme';
+
+export const Wrapper = styled.div`
+  max-width: 350px;
+`;
+
+export const MonthWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  margin: 0 auto;
+  margin-bottom: 31px;
+
+  width: 101px;
+`;
+
+export const IconWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const DayWrapper = styled.td<{ isWeekend: boolean; isClicked: boolean }>`
+  cursor: pointer;
+
+  width: 50px;
+  height: 70px;
+
+  border-bottom: 0.5px solid #eeeeee;
+  background-color: ${Theme.B_1};
+  box-shadow: ${({ isClicked }) => isClicked && `0 0 0 3px ${Theme.M_5} inset`};
+  border-radius: 5px;
+
+  padding: 10px 0px 0px 10px;
+
+  font-size: 20px;
+  color: ${({ isWeekend }) => (isWeekend ? Theme.T_2 : Theme.T_1)};
+`;
