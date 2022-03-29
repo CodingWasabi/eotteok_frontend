528f14901769aa05a8ad59346c0608e29231d4a7
yoon-junseo <ysj6844@naver.com>
2022-03-29 11:54:29 +0900

    feat: useSurveyActions에 action 추가
    #12

--- src/hooks/useSurveyActions.ts
+++ src/hooks/useSurveyActions.ts
@@ -2,20 +2,36 @@ import { useCallback } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import { handleAnswerList } from '@/modules/survey';
-import { AnswerListType, SubjectInfoListType } from '@/modules/survey';
+import {
+  handleAnswerList,
+  updateIsFilled,
+  updateExamSchedule,
+  registerExamInfo,
+  initializeExamInfo,
+  updateExamInfoId,
+  editExamInfo,
+} from '@/modules/survey';
 
-interface IUseSurveyActionsProps {
-  answerList?: AnswerListType;
-  subjectInfoList?: SubjectInfoListType;
-}
-
-const useSurveyActions = ({ answerList = [], subjectInfoList = [] }: IUseSurveyActionsProps) => {
+const useSurveyActions = () => {
   const dispatch = useDispatch();
 
-  const onClickAnswer = useCallback(() => dispatch(handleAnswerList(answerList)), [dispatch, answerList]);
+  const onClickAnswer = useCallback((answerList) => dispatch(handleAnswerList(answerList)), [dispatch]);
+  const dispatchIsFilled = useCallback((isFilled) => dispatch(updateIsFilled(isFilled)), [dispatch]);
+  const updateExamScheduleInfo = useCallback((schedule) => dispatch(updateExamSchedule(schedule)), [dispatch]);
+  const registerExam = useCallback((info) => dispatch(registerExamInfo(info)), [dispatch]);
+  const initExamInfo = useCallback(() => dispatch(initializeExamInfo()), [dispatch]);
+  const dispatchExamInfoId = useCallback((id) => dispatch(updateExamInfoId(id)), [dispatch]);
+  const editExam = useCallback((examInfo) => dispatch(editExamInfo(examInfo)), [dispatch]);
 
-  return { onClickAnswer };
+  return {
+    onClickAnswer,
+    dispatchIsFilled,
+    updateExamScheduleInfo,
+    registerExam,
+    initExamInfo,
+    dispatchExamInfoId,
+    editExam,
+  };
 };
 
 export default useSurveyActions;
