7fb2c5ea17168eb2f6b17926c454e993159d90a5
yoon-junseo <ysj6844@naver.com>
2022-03-29 12:17:01 +0900

    feat: SurveyPage 구성
    1. useWarningExit을 통해 페이지 종료 탐지
    2. 시험 목록 렌더링 추가
    #12

--- src/pages/SurveyPage/index.tsx
+++ src/pages/SurveyPage/index.tsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
+import useSurvey from '@/hooks/useSurvey';
+import useWarngingExit from '@/hooks/useWarningExit';
+
 import ProgressBar from '@/components/Survey/ProgressBar';
 import QuestionSelection from '@/components/Survey/QuestionSelection';
 
-import AppLayout from '@/components/common/AppLayout';
+import ExamItem from '@/components/Exam/ExamItem';
 
-import { Body } from './style';
+import AppLayout from '@/components/common/AppLayout';
+import Button from '@/components/common/Button';
+
+import { ExamInfoType } from '@/modules/survey';
+
+import { Body, ExamInfoListWrapper, ExamListContainer, TextWrapper } from './style';
 
 const SurveyPage = () => {
+  const { examInfoList, isEdit, examInfoId } = useSurvey();
+
   const [percentage, setPercentage] = useState<number>(0);
   const [progressStep, setProgressStep] = useState<number>(0);
 
@@ -15,12 +25,33 @@ const SurveyPage = () => {
     setPercentage((progressStep / 5) * 100);
   }, [progressStep]);
 
+  useWarngingExit();
+
   return (
     <AppLayout>
       <Body>
         <ProgressBar percentage={percentage} />
         <QuestionSelection progressStep={progressStep} setProgressStep={setProgressStep} />
       </Body>
+      {progressStep === 4 && (
+        <ExamInfoListWrapper>
+          {examInfoList.length > 0 ? (
+            <ExamListContainer>
+              {examInfoList.map((exam: ExamInfoType, index: number) => (
+                <ExamItem key={index} id={index} isEdit={isEdit} isSelected={examInfoId === index}>
+                  {exam.exam}
+                </ExamItem>
+              ))}
+            </ExamListContainer>
+          ) : (
+            <TextWrapper>
+              시험정보를 등록하면 <br />
+              리스트를 볼 수 있어요!
+            </TextWrapper>
+          )}
+          <Button variant="M_2">넘기시지</Button>
+        </ExamInfoListWrapper>
+      )}
     </AppLayout>
   );
 };
