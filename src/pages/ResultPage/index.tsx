c6b06d7293d2bf9cfc3db4bac4174f15aacb2fc3
yoon-junseo <ysj6844@naver.com>
2022-03-31 08:38:31 +0900

    feat: 결과 페이지에 요소 추가
    1. 댓글 입력창
    2. 달력 공유해 보시지 버튼
    3. 초기화 하기 버튼
    #17

--- src/pages/ResultPage/index.tsx
+++ src/pages/ResultPage/index.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Calendar from '@/components/Calendar';
 
-import AppLayout from '@/components/common/AppLayout';
+import CommentInput from '@/components/Comment/CommentInput';
 
-import { Body } from './style';
+import AppLayout from '@/components/common/AppLayout';
+import Button from '@/components/common/Button';
+import Text from '@/components/common/Text';
+import Icon from '@/components/Icon';
+
+import { Theme } from '@/styles/Theme';
+
+import { Body, ButtonWrapper, ResetText, CommentInputWrapper } from './style';
 
 const ResultPage = () => {
+  const [isClickedDate, setIsClickedDate] = useState<number>(0);
+
+  const hasComments = true;
+
+  const onClickRegisterComment = () => {
+    alert('댓글 등록');
+  };
+
   return (
     <AppLayout>
       <Body>
-        <Calendar />
+        <Calendar isClickedDate={isClickedDate} setIsClickedDate={setIsClickedDate} />
+        <CommentInputWrapper hasComments={hasComments}>
+          {!hasComments && (
+            <Text fontSize={16} letterSpacing={-0.5}>
+              날짜 선택하고 댓글을 남겨보시지 !
+            </Text>
+          )}
+          <CommentInput hasComments={hasComments} onClickButton={onClickRegisterComment} />
+        </CommentInputWrapper>
+        <ButtonWrapper>
+          <Button variant="M_4">
+            <Icon icon="Share" />
+            <Text color={Theme.B_1}>달력 공유해 보시지</Text>
+          </Button>
+        </ButtonWrapper>
+        <ResetText>초기화 하기</ResetText>
       </Body>
     </AppLayout>
   );
--- src/pages/ResultPage/style.ts
+++ src/pages/ResultPage/style.ts
@@ -1,8 +1,48 @@
 import styled from 'styled-components';
 
+import { Theme } from '@/styles/Theme';
+
 export const Body = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  height: 100vh;
+`;
+
+export const ButtonWrapper = styled.div`
+  margin-bottom: 120px;
+`;
+
+export const ResetText = styled.span`
+  cursor: pointer;
+
+  margin-bottom: 80px;
+
+  color: ${Theme.T_1};
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.5px;
+
+  text-decoration: underline;
+  text-underline-position: under;
+`;
+
+export const CommentInputWrapper = styled.div<{ hasComments: boolean }>`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+
+  background-color: ${Theme.B_1};
+
+  margin: 17px 0 30px 0;
+  padding: 12px 0;
+
+  & > :first-child {
+    margin-bottom: ${({ hasComments }) => !hasComments && '20px'};
+  }
+
+  width: 100%;
 `;
