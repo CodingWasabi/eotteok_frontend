8e35c92884e64537185a111f66a0f5228f433f6c
yoon-junseo <ysj6844@naver.com>
2022-04-04 22:08:18 +0900

    feat: tendencyList 추가
    #17

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as colorList } from './colorList';
 export { default as questionList } from './questionList';
 export { default as prepareTimeList } from './prepareTimeList';
+export { default as tendencyList } from './tendencyList';
