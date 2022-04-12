be2a1b24f0c7b178d87ab4a1192de4d26813c4e0
yoon-junseo <ysj6844@naver.com>
2022-04-12 09:28:41 +0900

    fix: api/client
    1. withCredential 제거 -> cors 에러 때문에 제거함
    #21

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -5,6 +5,5 @@ export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 const client = axios.create();
 
 client.defaults.baseURL = SERVER_URL;
-client.defaults.withCredentials = true;
 
 export default client;
