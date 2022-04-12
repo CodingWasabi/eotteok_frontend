1df926b9b3333517a3f8d2d71bd52988ca33d43e
yoon-junseo <ysj6844@naver.com>
2022-04-12 22:44:59 +0900

    feat: loginPath 변수 추가
    1. 상수 추가
    #21

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -2,3 +2,4 @@ export { default as colorList } from './colorList';
 export { default as questionList } from './questionList';
 export { default as prepareTimeList } from './prepareTimeList';
 export { default as tendencyList } from './tendencyList';
+export { default as loginPath } from './loginPath';
---
+++ src/lib/constants/loginPath.ts
@@ -0,0 +1,4 @@
+const loginPath =
+  'https://kauth.kakao.com/oauth/authorize?client_id=8cbb1d014182769153fc5832fced3743&redirect_uri=http://3.34.94.220:8080/login/oauth2/code/kakao&response_type=code';
+
+export default loginPath;
