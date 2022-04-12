ec2f0a94b243e7cd7553eb8fe7ec98a204aba7fb
yoon-junseo <ysj6844@naver.com>
2022-04-12 22:38:24 +0900

    feat: api/calendar 함수 추가
    1. postCalendar
    2. getPersonalCalendar
    3. postComment
    #21

--- src/lib/api/calendar.ts
+++ src/lib/api/calendar.ts
@@ -1,14 +1,19 @@
 import client from '@/lib/api/client';
 
-import { IRequestCalendar, IRequestCommentParams, IRequestPostComment } from '@/types/calendar';
+import { IRequestCalendar, IRequestCommentParams, IRequestPostComment, IResponsePostCalendar } from '@/types/calendar';
 
-export const registerCalendar = ({ ...body }: IRequestCalendar) => client.post('/calendar', body);
+export const getPersonalCalendar = (userId: number) => client.get(`/calendar/${userId}/result`).then((res) => res.data);
 
 export const getComments = ({ userId, date }: IRequestCommentParams) =>
-  client.get(`/calendar/${userId}/result/comments?date=${date}`);
+  client.get(`/calendar/${userId}/result/comments?date=${date}`).then((res) => res.data);
+
+export const postCalendar = ({ ...body }: IRequestCalendar) =>
+  client.post<IResponsePostCalendar>('/calendar', body).then((res) => res.data);
 
 export const postComment = ({ userId, date, profileImageNumber, body }: IRequestPostComment) =>
-  client.post(`/calendar/${userId}/result/comments/${date}`, {
-    profileImageNumber,
-    body,
-  });
+  client
+    .post(`/calendar/${userId}/result/comments/${date}`, {
+      profileImageNumber,
+      body,
+    })
+    .then((res) => res.data);
