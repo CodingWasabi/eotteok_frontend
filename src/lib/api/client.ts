b17f35c9c1041b7c45f8a6545699a5bb88ece5ed
yoon-junseo <ysj6844@naver.com>
2022-04-13 13:43:13 +0900

    feat: cookie 추가
    1. cookie 추가 테스트
    #21

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const client = axios.create();
 
+const cookies = new Cookies();
+
+const J_SESSION_ID = cookies.get('JSESSIONID');
+
+document.cookie = `JSESSIONID=${J_SESSION_ID}`;
+console.log(document.cookie);
+
 client.defaults.baseURL = SERVER_URL;
+client.defaults.withCredentials = true;
 
 export default client;
