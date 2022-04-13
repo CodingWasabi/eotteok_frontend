01c3d918a24c7ffeba321e0c7a39b44fa9469caa
yoon-junseo <ysj6844@naver.com>
2022-04-13 10:41:10 +0900

    feat: redux-persist 설정
    1. rootReducer에 설정
    2. index.tsx에 설정
    #21

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from '@redux-devtools/extension';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import rootReducer, { rootSaga } from '@/modules';
 
@@ -13,11 +15,15 @@ const SagaMiddleware = createSagaMiddleware();
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(SagaMiddleware)));
 
+const persistor = persistStore(store);
+
 SagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.querySelector('#root'),
 );
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,10 +1,17 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
 import { all } from 'redux-saga/effects';
+import storage from 'redux-persist/lib/storage';
 
 import survey from '@/modules/survey';
 import nickname from '@/modules/nickname';
 import comment from '@/modules/comment';
 import calendar, { calendarSaga } from '@/modules/calendar';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
   survey,
@@ -13,7 +20,7 @@ const rootReducer = combineReducers({
   calendar,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
 
 export type RootState = ReturnType<typeof rootReducer>;
 
