2177b7c331dccd70d87d6a050c742454fae4904e
yoon-junseo <ysj6844@naver.com>
2022-04-13 15:21:08 +0900

    fix: client.ts
    1. universal-cookie 관련 삭제
    #21

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,15 +1,8 @@
 import axios from 'axios';
-import Cookies from 'universal-cookie';
 
 export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const client = axios.create();
-
-const cookies = new Cookies();
-
-const J_SESSION_ID = cookies.get('JSESSIONID');
-
-document.cookie = `JSESSIONID=${J_SESSION_ID}`;
 
 client.defaults.baseURL = SERVER_URL;
 client.defaults.withCredentials = true;
