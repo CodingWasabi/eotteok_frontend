e182c456b23e4a9f3bb01bc4b1b55a07756cff41
yoon-junseo <ysj6844@naver.com>
2022-04-14 10:29:17 +0900

    feat: api/my에 타입 추가
    1. getMyCalendar에 response 타입 추가
    #21

--- src/lib/api/my.ts
+++ src/lib/api/my.ts
@@ -1,8 +1,9 @@
 import client from '@/lib/api/client';
 
 import { IExam } from '@/types/exam';
+import { IResponsePostCalendar } from '@/types/calendar';
 
-export const getMyCalendar = () => client.get('/my/calendar/result').then((res) => res.data);
+export const getMyCalendar = () => client.get<IResponsePostCalendar>('/my/calendar/result').then((res) => res.data);
 
 export const getMyExam = () => client.get('/my/exam').then((res) => res.data);
 
