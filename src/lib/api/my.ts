8516b552fb1841ce6aae1eb41b309f73f3e1897f
yoon-junseo <ysj6844@naver.com>
2022-04-21 08:30:52 +0900

    fix: api/my
    1. getMyExam 함수명 수정
    2. response 타입 지정
    3. 인자 추가
    #21

--- src/lib/api/my.ts
+++ src/lib/api/my.ts
@@ -1,10 +1,12 @@
 import client from '@/lib/api/client';
 
+import { ExamInfoListType } from '@/modules/survey';
+
 import { IExam } from '@/types/exam';
 import { IResponsePostCalendar } from '@/types/calendar';
 
 export const getMyCalendar = () => client.get<IResponsePostCalendar>('/my/calendar/result').then((res) => res.data);
 
-export const getMyExam = () => client.get('/my/exam').then((res) => res.data);
+export const getMyExamList = (id: number) => client.get<ExamInfoListType>('/my/exam').then((res) => res.data);
 
 export const putMyExam = (body: Array<IExam>) => client.put('/my/exams', { exams: body });
