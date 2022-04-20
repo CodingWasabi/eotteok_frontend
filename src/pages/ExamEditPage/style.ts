24efbf982e52b4eb55a9e85dbaef4cf535d74ca6
yoon-junseo <ysj6844@naver.com>
2022-04-20 17:46:59 +0900

    feat: ExamEditPage에 기능 추가
    1. 기능 추가
    2. 디자인 추가
    #21

--- src/pages/ExamEditPage/style.ts
+++ src/pages/ExamEditPage/style.ts
@@ -7,6 +7,76 @@ export const Body = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
 
-  height: 100vh;
+export const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > :not(:last-child) {
+    margin-bottom: 25px;
+  }
+
+  & > :first-child {
+    margin: 45px 0px;
+  }
+`;
+
+export const DelayedTimeWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 325px;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  margin-top: 50px;
+`;
+
+export const TextCenterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin: 20px 0 10px 0;
+`;
+
+export const ExamInfoListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: ${Theme.B_4};
+
+  width: 100%;
+  min-height: 194px;
+
+  & > :last-child {
+    margin: 50px 0 20px 0;
+  }
+`;
+export const ExamListContainer = styled.div`
+  & > :first-child {
+    margin-top: 20px;
+  }
+
+  & > :not(:first-child) {
+    margin-top: 10px;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  text-align: center;
+
+  font-size: 24px;
+  color: ${Theme.T_3};
+
+  margin: 70px 0;
 `;
