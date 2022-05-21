240b9b0c52700a5e78c54a473fac989ae73b6f04
jun5e00 <ysj6844@naver.com>
2022-05-21 22:22:54 +0900

    feat: webpack.dev
    1. allowedHosts 추가
    #26

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,7 @@ module.exports = merge(common, {
     hot: true,
     port: 3000,
     historyApiFallback: true,
+    allowedHosts: ['how-to-study.kro.kr'],
   },
   plugins: [
     new HtmlWebpackPlugin({
