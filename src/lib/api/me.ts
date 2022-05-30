27a206b38d00872c2f0df1d7a5f4fae3d87f339a
jun5e00 <ysj6844@naver.com>
2022-05-31 08:26:24 +0900

    feat: reset api
    1. 초기화 api 추가
    #26

--- src/lib/api/me.ts
+++ src/lib/api/me.ts
@@ -1,3 +1,5 @@
 import client from '@/lib/api/client';
 
-export const getMe = () => client.get('me').then((res) => res.data);
+export const getMe = () => client.get('/me').then((res) => res.data);
+
+export const resetMe = () => client.delete('/me');
